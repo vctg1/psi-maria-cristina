71d54b217e1b8a6101f0d4a4fb9b1f9574539574
vctg1 <victormourago@gmail.com>
2025-09-27 17:36:21 -0300

    ignore eslint warnings

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -20,6 +20,13 @@ const eslintConfig = [
       "next-env.d.ts",
     ],
   },
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "@next/next/no-img-element": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
