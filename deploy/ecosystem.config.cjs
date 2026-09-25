be2a53c8f11b115b78cfcccbd9047cd93a39dd13
vctg1 <victormourago@gmail.com>
2026-09-25 17:22:12 -0300

    Use lint-compatible PM2 release paths

--- deploy/ecosystem.config.cjs
+++ deploy/ecosystem.config.cjs
@@ -1,12 +1,11 @@
-const path = require('node:path');
-const appDir = process.env.PSI_APP_DIR || path.resolve(__dirname, '..');
+const appDir = process.env.PSI_APP_DIR || __dirname + '/..';
 
 module.exports = {
   apps: [
     {
       name: 'psi-maria-cristina',
       cwd: appDir,
-      script: path.join(appDir, 'node_modules/next/dist/bin/next'),
+      script: appDir + '/node_modules/next/dist/bin/next',
       args: 'start --hostname 127.0.0.1 --port 3010',
       interpreter: '/root/.nvm/versions/node/v24.18.1/bin/node',
       instances: 1,
